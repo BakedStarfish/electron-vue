5a7b8366115e49fa95bc5903f88865ae9dfeb9b2
liangxing@c5game.com <liangxing@c5game.com>
2020-04-28 14:07:20 +0800

    项目

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'testc5'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
